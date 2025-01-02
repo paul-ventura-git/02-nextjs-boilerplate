9b91093c4e4ad6df4211ed777c75fa0fb1a7438c
paul-ventura-git <j.paul.ventura@gmail.com>
2025-01-02 17:27:00 -0500

    Header

---
+++ app/header.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <>
+      <img src="globe.svg" alt="Logo" />
+      <h1>My First NextJS Website</h1>
+    </>
+  )
+}
